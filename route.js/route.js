30fe403fee4d54f971fbeecb23a694a11d44edc2
Bea CP <86390789+beaodara@users.noreply.github.com>
2023-05-29 22:16:29 +0000

    Create route.js

---
+++ route.js/route.js
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DadosService {
+  private apiUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  getDados(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/seu-endpoint`);
+  }
+}
+
+
+//
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DadosService {
+  private apiUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  getDados(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/seu-endpoint`);
+  }
+}
+
